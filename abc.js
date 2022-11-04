00dad67a756553e9be54dc99f4616150523e36d7
tamdc2704 <tamdc2704@gmail.com>
2022-11-04 23:22:12 +0700

    Add comment to fix bug

--- abc.js
+++ abc.js
@@ -4,7 +4,7 @@ const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/users';
 const a = document.getElementById("root");
 axios.get(API_ENDPOINT);
 
-async function main(){
+async function main() {
     const data = await axios.get(API_ENDPOINT);
     console.log(data);
 
@@ -18,24 +18,24 @@ function hideloader() {
     document.getElementById('loading').style.display = 'none';
 }
 
-function show(data) {
-    let tab = 
+function show(data) { // Khai báo hàm show nhưng đã sử dụng ở đâu?
+    let tab =
         `<tr>
           <th>ID</th>
           <th>Name</th>
           <th>Phone</th>
           <th>Username</th>
          </tr>`;
-    
+
     // Loop to access all rows 
-    array.forEach(r => {
+    array.forEach(r => { // biến array này ở đâu ra mà sài đây?
         tab += `<tr> 
         <td>${r.id} </td>
         <td>${r.name}</td>
         <td>${r.phone}</td> 
         <td>${r.username}</td>          
     </tr>`;
-    }); 
+    });
     // Setting innerHTML as tab variable
     document.getElementById("users").innerHTML = tab;
 }
